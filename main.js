3540ece5ed020403bf8a9c7a3f0b6c1724a9e32e
unknown <mihai@hotmail.be>
2020-02-17 15:29:56 +0100

    adde animation

---
+++ main.js
@@ -0,0 +1,9 @@
+let graphMax = document.getElementById('maxTemp');
+let graphMin = document.getElementById('minTemp');
+
+
+function executeFunction() {
+    graphMax.beginElement();
+    graphMin.beginElement();
+
+}
